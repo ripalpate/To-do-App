2b4d02ad2e9ce2b2a545e90d17000cceef58e470
Ripal Patel <ripal212007@gmail.com>
2018-11-14 20:08:00 -0600

    login works

--- src/components/Auth/auth.js
+++ src/components/Auth/auth.js
@@ -1,11 +1,21 @@
 import $ from 'jquery';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import googleImage from './google-login-button.png';
 import './auth.scss';
 
 const loginButton = () => {
   const domString = `
-  <button class="btn btn-secondary">Log in</button>
+  <button id="google-auth" class="btn btn-warn">
+  <img src="${googleImage}" src= "Login Button" width ="350px;">
+  </button>
   `;
   $('#login-container').html(domString);
+  $('#google-auth').on('click', () => {
+    // console.log('clicked');
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  });
 };
 
 export default loginButton;
