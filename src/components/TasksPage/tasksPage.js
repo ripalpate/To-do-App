0b4cdf3833dde6b15be681efbf242c4fd059e2cc
Ripal Patel <ripal212007@gmail.com>
2018-11-21 23:58:42 -0600

    completed task is done

--- src/components/TasksPage/tasksPage.js
+++ src/components/TasksPage/tasksPage.js
@@ -6,12 +6,13 @@ const printAllTasks = (tasksArray) => {
   let domString = '';
   domString += '<h5 class="task-heading">Tasks </h5>';
   tasksArray.forEach((task) => {
-    domString += `<div class="input-group-text w-25 task">
-                    <input type="checkbox" class="task-checkbox">
-                    <p class="task-desc">${task.task}<p>
-                  </div>
-                  `;
-    $('#tasks').html(domString);
+    if (task.isCompleted === false) {
+      domString += `<div class="input-group-text w-25 task">
+                      <input type="checkbox">
+                      <p class="task-desc" data-task-id=${task.id}>${task.task}<p>
+                    </div>`;
+      $('#tasks').html(domString);
+    }
   });
 };
 
@@ -24,10 +25,40 @@ const tasksPage = () => {
     });
 };
 
-$('body').on('click', 'input[type=checkbox]', (e) => {
-  const taskToMove = $(e.target).closest('.task').text();
-  $('#completed-tasks').append(`<div>${taskToMove}</div>`);
-});
+const gettingTaskFromList = (iscompleted, elementToUpdate) => {
+  const task = {
+    task: $(elementToUpdate).text(),
+    isCompleted: iscompleted,
+  };
+  return task;
+};
+
+const completedTask = (e) => {
+  const element = e.target;
+  element.classList.toggle('checked');
+  const iscompleted = $(element).hasClass('checked');
+  const elementToUpdate = $(e.target).siblings('p')[0];
+  const idToUpdate = elementToUpdate.dataset.taskId;
+  const updatedtaskObject = gettingTaskFromList(iscompleted, elementToUpdate);
+  tasksData.updateSingleTask(updatedtaskObject, idToUpdate)
+    .then(() => {
+      if (iscompleted) {
+        const taskToMove = $(e.target).closest('.task').text();
+        $('#completed-tasks').append(`<div id="${idToUpdate}-done">${taskToMove}</div>`);
+        $(elementToUpdate).css('text-decoration', 'line-through');
+      } else {
+        let incompleteTaskId = '#';
+        incompleteTaskId += idToUpdate;
+        incompleteTaskId += '-done';
+        $(incompleteTaskId).remove();
+        $(elementToUpdate).css('text-decoration', 'none');
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+};
+
+$('body').on('click', 'input[type=checkbox]', completedTask);
 
 
-export default tasksPage;
+export default { tasksPage };
